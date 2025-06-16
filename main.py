dde96a891d8de705c831160ff7cfcfbbb9cd7932
unknown <ig.vasylenko2@gmail.com>
2025-06-16 21:49:13 +0300

    main.py cleared

--- main.py
+++ main.py
@@ -2,19 +2,11 @@ import asyncio
 from instagram_checker import check_unread_messages
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import Message
-from aiogram.utils import executor
 import os
 from dotenv import load_dotenv
-from utils.caption_writer import generate_caption
 
 load_dotenv()
 bot = Bot(token=os.getenv("BOT_TOKEN"))
-dp = Dispatcher(bot)
-
-@dp.message_handler(content_types=types.ContentType.PHOTO)
-async def handle_photo(msg: Message):
-    caption = generate_caption()
-    await msg.answer(f"✨ Предложение поста:\n\n{caption}")
 
 
 async def periodic_check() -> None:
@@ -30,4 +22,3 @@ if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.create_task(periodic_check())
     print("🚀 Bot is running...")
-    executor.start_polling(dp, loop=loop)
--- utils/instagram_checker.py
+++
@@ -1,9 +0,0 @@
-import os
-import requests
-from dotenv import load_dotenv
-
-IG_ACCESS_TOKEN = os.getenv("IG_ACCESS_TOKEN")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
-
-### s
