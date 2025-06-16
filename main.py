8a2157e883686e8f3890a713b484bc6498a0f2a5
karonion11 <ig.vasylenko2@gmail.com>
2025-06-16 21:54:25 +0300

    Clean up main and run periodic check

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 import asyncio
 from instagram_checker import check_unread_messages
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import Message
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-bot = Bot(token=os.getenv("BOT_TOKEN"))
 
 
 async def periodic_check() -> None:
@@ -19,6 +12,8 @@ async def periodic_check() -> None:
         await asyncio.sleep(300)
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.create_task(periodic_check())
     print("🚀 Bot is running...")
+    try:
+        asyncio.run(periodic_check())
+    except KeyboardInterrupt:
+        pass
